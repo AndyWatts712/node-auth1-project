d70fdc23c0d101ed3286f6cdb264ab7741159a84
megawatts68 <andrew.f.watts@gmail.com>
2020-09-15 02:47:15 +0700

    Database and seed created.

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const router = require('../auth/auth-router')
 const authRouter = require('../auth/auth-router')
 const server = express()
 
 server.use(express.json())
 
-server.use('/api/auth', authRouter)
-
+server.use('/api/', authRouter)
 
 
 module.exports = server
